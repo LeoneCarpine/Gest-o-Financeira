88b8c89cea82cd42a83081faddf305702ef50bdf
Felipe Arosti <118208364+felipearosti@users.noreply.github.com>
2025-04-07 19:18:11 -0300

    Update Main.java
    Adicionando Login

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
+
+        GestorUsuarios gestorUsuarios = new GestorUsuarios();
         GestorTransacoes gestorTransacoes = new GestorTransacoes();
         GestorCategoria gestorCategoria = new GestorCategoria();
-        new MenuPrincipalGUI(gestorTransacoes, gestorCategoria);        // interface
+
+        // Add adm
+        gestorUsuarios.registrarUsuario("adm", "Admin4123", "Administrador");
+
+        CadastroLoginGUI loginGUI = new CadastroLoginGUI(gestorUsuarios);
+        loginGUI.exibir();
     }
 }
