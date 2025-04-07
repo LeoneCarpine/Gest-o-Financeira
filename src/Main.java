6e4f481e5119fde5da98aaa007f732930bb7290b
Felipe Arosti <118208364+felipearosti@users.noreply.github.com>
2025-04-07 18:54:54 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Date;
+
 public class Main {
-    new PrimeiraTela();
+    public static void main(String[] args) {
+        GestorTransacoes gestorTransacoes = new GestorTransacoes();
+        GestorCategoria gestorCategoria = new GestorCategoria();
+        new MenuPrincipalGUI(gestorTransacoes, gestorCategoria);        // interface
+    }
 }
